a131bcd65c69a7060ca6ad7770543ee8f1587974
Randal <Randalgamboa.16@gmail.com>
2020-01-27 20:06:36 -0400

    Practica Básica de Rutas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">About</router-link> 
+      <router-link to="/services">Services</router-link> 
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+ background-color: rgba(0, 0, 0, 0.658);
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 
 #nav {
   padding: 30px;
+  background-color: #42b983;
+  margin-bottom: 20px;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding:0px 20px
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  border-bottom: 1px solid #2c3e50;
+  color:black;
 }
+
 </style>
---
+++ src/views/Services.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pb-5">
+    <h1>Estos son los servicios :</h1>
+
+      <router-link :to="{ name: 'services', params:{ id: items} }" 
+        
+        v-for="(items , index) of servicios" :key="index" >
+
+            <button class="btn btn-outline-success px-5 mx-2">{{items}}</button>
+
+        </router-link>
+
+    <Servicio class="my-5"/>
+
+        <button class="btn btn-primary px-5 mx-2" @click="anterior">Anterior</button>
+        <button class="btn btn-info px-5 mx-2" @click="home">Home</button>
+        <button class="btn btn-primary px-5 mx-2" @click="siguiente">Siguiente</button>
+  </div>
+
+</template>
+
+<script>
+
+  import Servicio from '@/components/Servicio.vue'
+
+export default {
+  name: "Servicios",
+  components:{
+    Servicio
+  },
+   data() {
+        return {
+            servicios :[1,2,3]
+        }
+    },methods: {
+      home(){
+        this.$router.push("/")
+      },
+      anterior(){
+        this.$router.go(-1)
+      },
+      siguiente(){
+        this.$router.go(1)
+      }
+    }
+}
+</script>
